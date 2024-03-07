2c27fe10abcc12897e64596842a53a9b4605f583
Karol Kawski <karol.kawski@protonmail.com>
2024-03-07 20:59:45 +0100

    [update add font awesome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,9 @@
-// import './index.scss';
-// import axios from 'axios';
-// import HabitContext from './store/context';
-// import SettingsContext from './store/settings';
-// import { useContext, useEffect, useState } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Habit } from './pages/habit';
-// import { SettingsProvider } from './Providers/SettingsProvider';
 import { Add } from './pages/add';
-// import { Dashboard } from './pages/dashboard';
 import { Edit } from './pages/edit';
 import { Root } from './pages/root';
-// import { HabitType } from './types/Habit.d';
 import { Habits } from './pages/habits';
-// import { Header } from './Layout/Header/Header';
 import { Statistics } from './pages/statistics';
 import { Settings } from './pages/settings';
 import { Login } from './pages/login';
--- frontend/src/components/UI/Icon/Icon.tsx
+++ frontend/src/components/UI/Icon/Icon.tsx
@@ -3,7 +3,7 @@ import './Icon.scss';
 export const Icon = ({ icon }: { icon: string }) => {
   return (
     <div className="Icon">
-      <i className={'fa-solid ' + icon}></i>
+      <i className={'fa-solid fa-' + icon}></i>
     </div>
   );
 };
